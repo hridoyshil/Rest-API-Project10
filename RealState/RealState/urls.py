8e80f08f46a3de645ed2a2ccb50743bbc0246706
Hridoy <hriodykumarshil12@gmail.com>
2023-10-23 11:59:52 +0600

    Login API | Create Project |
    https://www.youtube.com/watch?v=Yi8t6kU68uk&list=PL4NIq30KvXLDES6CUeAWiSJNQzPsoBWI6&index=23

--- RealState/RealState/urls.py
+++ RealState/RealState/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
