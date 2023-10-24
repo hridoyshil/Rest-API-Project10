7cfa77e8ab84483f6f202b21dfa072e9bf6c32f5
Hridoy <hriodykumarshil12@gmail.com>
2023-10-25 00:31:23 +0600

    Buildings App & Related API | Django REST Framework--multi_ImageView
    https://www.youtube.com/watch?v=hLg8gwy6MOU&list=PL4NIq30KvXLDES6CUeAWiSJNQzPsoBWI6&index=27

---
+++ RealState/buildings/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Home, ImageFiles
+
+admin.site.register(Home)
+admin.site.register(ImageFiles)
---
+++ RealState/buildings/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from rest_framework import permissions, serializers, status
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
+from .models import Home, ImageFiles
+from .serializers import HomeSerializer, HomeDetailSerializer, ImageFilesSerializer
+
+
+class HomeListAPIView(ListAPIView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = HomeSerializer
+    queryset = Home.objects.filter(is_published=True).order_by("-list_date")
+    lookup_field = "slug"
+
+
+class HomeDetailAPIView(RetrieveAPIView):
+    serializer_class = HomeDetailSerializer
+    queryset = Home.objects.filter(is_published=True).order_by("-list_date")
+    lookup_field = "slug"
+
+
+class ImageView(APIView):
+    def get(self, request, pk, format=None):
+        home = Home.objects.get(pk=pk)
+        images = home.images.all()
+        serializer = ImageFilesSerializer(images, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
